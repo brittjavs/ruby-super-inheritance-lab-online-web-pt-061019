2d44f2ca1c236c92413bb36a3663a813f73867cf
Brittany Javalera <brittjavalera@gmail.com>
2019-07-15 21:32:34 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -4,12 +4,6 @@ class ChattyStudent < Student
     puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end
   def raise_hand
-   i = 0
-    loop do
-      i += 1
-      super
-      if i == 10
-  break         # this will cause execution to exit the loop
-end
-end
+   
+  end
 end
